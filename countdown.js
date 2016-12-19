574a3c358c037584c1611e5636642bb3ff7af66f
novayoub <santayoub@gmail.com>
2016-12-19 22:04:40 +0000

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,40 @@
+﻿
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(endtime){
+  var clock       = document.getElementById('counter');
+  var daysSpan    = document.getElementById('days');
+  var hoursSpan   = document.getElementById('hours');
+  var minutesSpan = document.getElementById('minutes');
+  var secondsSpan = document.getElementById('seconds');
+
+	// update the clock in html code
+  function updateClock(){
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock,1000);
+}
